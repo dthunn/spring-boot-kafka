0fdd7d06ac05e58dbfb49dc06a637f6d6f493451
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-10-27 09:54:35 -0600

    error handling

---
+++ EmailNotificationMicroservice/src/main/java/com/app/email/notification/emailnotificationmicroservice/error/NotRetryableException.java
@@ -0,0 +1,15 @@
+package com.app.email.notification.emailnotificationmicroservice.error;
+
+public class NotRetryableException extends RuntimeException {
+
+    public NotRetryableException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public NotRetryableException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}
